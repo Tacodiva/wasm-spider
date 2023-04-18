48eee77f0c5e070aa21b7948e9bf2228d20c6e9d
Tacodiva <27910867+Tacodiva@users.noreply.github.com>
2023-04-18 23:49:00 +1000

    Import specs tests  Now, just to make them pass xD

--- tests/30_read_write/read_write.test.ts
+++ tests/30_read_write/read_write.test.ts
@@ -3,18 +3,28 @@ import path from 'path';
 import { spider } from '../../src';
 
 describe('Spider', () => {
-    describe('Read / Write', () => {
-        for (const file of fs.readdirSync("./tests/bin")) {
-            const wasmPath = path.join("./tests/bin", file);
-            const wasmName = path.basename(wasmPath, ".wasm");
-            test(wasmName, async () => {
-                const wasmInput = new Uint8Array(fs.readFileSync(wasmPath));
-                const spiderModule = spider.readModule(wasmInput);
-                const wasmOutput = spider.writeModule(spiderModule, { mergeTypes: false });
 
-                await WebAssembly.compile(wasmOutput);
-                expect(wasmInput.buffer).toStrictEqual(wasmOutput.buffer);
-            });
-        }
-    });
+    function addTests(dir: string, exact: boolean) {
+        describe(dir, () => {
+            for (const file of fs.readdirSync(dir)) {
+                if (!file.endsWith(".wasm")) continue;
+                const wasmPath = path.join(dir, file);
+                const wasmName = path.basename(wasmPath, ".wasm");
+                test(wasmName, async () => {
+                    const wasmInput = new Uint8Array(fs.readFileSync(wasmPath));
+                    const spiderModule = spider.readModule(wasmInput);
+                    const wasmOutput = spider.writeModule(spiderModule, { mergeTypes: false });
+
+                    if (exact)
+                        expect(wasmInput.buffer).toStrictEqual(wasmOutput.buffer);
+                    else
+                        expect(await WebAssembly.compile(wasmOutput));
+                });
+            }
+        });
+    }
+
+    addTests("./tests/bin", true);
+    addTests("./tests/specs", false);
+    addTests("./tests/specs/simd", false);
 });
